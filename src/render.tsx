40d6049d5bc19a9c676df3551d2ca07ab1c4a056
allexmorozovv <allexmorozovv@gmail.com>
2022-10-26 17:10:05 +0300

    add render.tsx

---
+++ src/render.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import {App} from './App';
+import {BrowserRouter} from "react-router-dom";
+import {addPost, AllPropsType} from "./Redux/state";
+
+export const rerenderTree = (state: AllPropsType) => {
+    ReactDOM.render(
+        <BrowserRouter>
+            <App state={state} addPost={addPost}/>
+        </BrowserRouter>,
+        document.getElementById('root')
+    );
+}
