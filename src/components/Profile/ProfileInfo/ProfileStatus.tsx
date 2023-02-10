1a99ad4cbe5588176452be274b1c0df2c5977908
allexmorozovv <allexmorozovv@gmail.com>
2023-02-10 15:17:01 +0300

    add componentDidUpdate

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -1,15 +1,15 @@
 import React, {ChangeEvent} from "react";
 import s from "./ProfileInfo.module.css"
 
-type ProfileStatusPropsType={
-    status:string
+type ProfileStatusPropsType = {
+    status: string
     updateUserStatus: (status: string) => void
 }
 
 class ProfileStatus extends React.Component  <ProfileStatusPropsType> {
     state = {
         editMode: false,
-        status:this.props.status
+        status: this.props.status
     }
 
     activateEditMode = () => {
@@ -26,8 +26,14 @@ class ProfileStatus extends React.Component  <ProfileStatusPropsType> {
         this.props.updateUserStatus(this.state.status)
     }
 
-    onStatusChange=(e:ChangeEvent<HTMLInputElement>)=>{
+    onStatusChange = (e: ChangeEvent<HTMLInputElement>) => {
         this.setState({status: e.currentTarget.value})
+    }
+
+    componentDidUpdate(prevProps: Readonly<ProfileStatusPropsType>, prevState: Readonly<{}>) {
+        if (prevProps.status !== this.props.status) {
+            this.setState({status: this.props.status})
+        }
     }
 
     render() {
@@ -35,12 +41,12 @@ class ProfileStatus extends React.Component  <ProfileStatusPropsType> {
             <div>
                 {!this.state.editMode &&
                     <div>
-                        <span onDoubleClick={this.activateEditMode}>{this.props.status|| '----------'}</span>
+                        <span onDoubleClick={this.activateEditMode}>{this.props.status || '----------'}</span>
                     </div>
                 }
                 {this.state.editMode &&
                     <div>
-                        <input onChange={this.onStatusChange}  autoFocus={true} type="text" value={this.state.status}
+                        <input onChange={this.onStatusChange} autoFocus={true} type="text" value={this.state.status}
                                onBlur={this.deactivateEditMode}/>
                     </div>
                 }
